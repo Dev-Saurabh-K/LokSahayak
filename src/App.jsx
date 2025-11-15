ce60dc0740db354ce0c337918a1174095f7bc7d9
Dev-Saurabh-K <saurabhkumar.sakr@gmail.com>
2025-11-15 14:26:19 +0530

    i18 lang

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { useTranslation } from "react-i18next";
 import axios from "axios";
+import i18n from "./i18n";
 
 
 const SpeechRecognition =
